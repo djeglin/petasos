2c2c05270ae0a141b4ff35fe4d2027881737fc54
David Eglin <david.eglin@mccann.com>
2019-01-03 11:08:48 +0000

    change argument binding

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const env = process.env.APPLICATION_ENV || process.env.NODE_ENV || 'dev'
 const nonProd = env !== 'production' && env !== 'prod' && env !== 'live'
 
 const Petasos = {
-  log: (nonProd) ? console.log : () => {},
+  log: function () {
+    if (nonProd) console.log(Array.prototype.slice.call(arguments))
+  },
 
   install (Vue, options) {
     Vue.prototype.log = () => {
